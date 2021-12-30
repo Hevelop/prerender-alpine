1895779c3c69a1e2fbf19b9604e578a0e00ed90c
Michele Lavarini <michele.lavarini@hevelop.com>
2021-12-30 09:42:56 +0100

    pageLoadTimeout: 40 * 1000

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const server = prerender({
     chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
     forwardHeaders: true,
-    chromeLocation: '/usr/bin/chromium-browser'
+    chromeLocation: '/usr/bin/chromium-browser',
+    pageLoadTimeout: 40 * 1000
 });
 
 server.use(require('prerender-aws-s3-cache'));
